8dc606760999d64a2b41c5d1c37dba4723ee7a97
mpanico20 <150909881+mpanico20@users.noreply.github.com>
2025-12-02 17:31:17 +0100

    Measures

--- Build/run_test.sh
+++ Build/run_test.sh
@@ -4,6 +4,9 @@ for size in 1MB 50MB 100MB 200MB 500MB
 do
     for threads in 1 2 4 8
     do
-        ./OpenMP_v1_O3 ../Data/string_$size.txt O3 $threads 1
+        ./OpenMP_v2_O0 ../Data/string_$size.txt O0 $threads 2
+        ./OpenMP_v2_O1 ../Data/string_$size.txt O1 $threads 2
+        ./OpenMP_v2_O2 ../Data/string_$size.txt O2 $threads 2
+        ./OpenMP_v2_O3 ../Data/string_$size.txt O3 $threads 2
     done
 done
