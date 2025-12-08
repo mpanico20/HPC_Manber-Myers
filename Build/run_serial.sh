ef2347fe9f0865f0d88ac4139f8e3c9edccc43d3
Marco Panico <m.panico20@studenti.unisa.it>
2025-12-08 16:28:53 +0100

    Measures CUDA

--- Build/run_serial.sh
+++ Build/run_serial.sh
@@ -2,6 +2,9 @@
 
 for size in 1MB 50MB 100MB 200MB 500MB
 do
-    ./sequential_O0 ../Data/string_$size.txt O0
+    ./CUDA_O0 ../Data/string_$size.txt O0
+    ./CUDA_O1 ../Data/string_$size.txt O1
+    ./CUDA_O2 ../Data/string_$size.txt O2
+    ./CUDA_O3 ../Data/string_$size.txt O3
 
 done
